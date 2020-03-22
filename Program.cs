f43fa1d1f560b34c62eaddd12afc723d5869dffb
Maria Rakova <maria.c.rakova@gmail.com>
2020-03-22 16:12:31 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Products firstProducts = new Products();
+            Products secondProducts = new Products("Coca-Cola");
+            Products thirdProducts = new Products("Fanta", 3);
+
+            double retail = firstProducts.retailPrice();
+
+            //Print a product, its unit of measure, its wholesale price and its retail price.
+
+            Console.WriteLine($"We have {firstProducts.Quantity} {firstProducts.UnitOfMeasure} of {firstProducts.ProductName}");
+            Console.WriteLine($"The wholesale is {firstProducts.WholesalePrice}" +
+                $" and the retail prices is {retail}");
+
+            Console.WriteLine($"We have {secondProducts.Quantity} {secondProducts.UnitOfMeasure} of {secondProducts.ProductName}");
+            Console.WriteLine($"The wholesale is {secondProducts.WholesalePrice}" +
+                $" and the retail prices is {retail}");
+
+
+            Console.WriteLine($"We have {thirdProducts.Quantity} {thirdProducts.UnitOfMeasure} of {thirdProducts.ProductName}");
+            Console.WriteLine($"The wholesale is {thirdProducts.WholesalePrice}" +
+                $" and the retail prices is {retail}");
+
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Definition_of_classes
-C# Definition of classes 
+
+## Exercise
+Make an encapsulated Products class that contains the following fields (items): 
+
+- product name, unit of measure, quantity, wholesale price.
+- Make more than 1 constructor and properties for each of the fields.
+- Create a method that calculate the price of some particulare product , according to the formula:
+
+`Retail Price = Wholesale Price + 20%.`
+
+- Print a product, its unit of measure, its wholesale price and its retail price.
